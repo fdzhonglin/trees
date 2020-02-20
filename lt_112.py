4659733aee7a74e04e4bdb697c06d2168c6270b9
Johnny Zhang <fd.zhonglin@gmail.com>
2020-02-19 17:23:25 -0800

    112. Path Sum

---
+++ lt_112.py
@@ -0,0 +1,35 @@
+# similar to question 111, leaf definition is changed a little bit
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        # leaf condition: in this example, become None node condition
+        if root == None:
+            return False
+         
+        # leaf condition by problem definition: 
+        if root.left == None and root.right == None:
+            if root.val == sum:
+                return True
+            
+            return False
+        
+        # path sum need to update before recursive call
+        sum = sum - root.val
+        if root.left == None:
+            return self.hasPathSum(root.right, sum)
+        
+        if root.right == None:
+            return self.hasPathSum(root.left, sum)
+        
+        # skeleton is here again :)
+        if self.hasPathSum(root.left, sum):
+            return True
+        
+        if self.hasPathSum(root.right, sum):
+            return True
+        
+        return False
