17ea985c3769790af5110254ee3689288977c1bc
Johnny Zhang <fd.zhonglin@gmail.com>
2020-02-19 16:09:25 -0800

    107. Binary Tree Level Order Traversal II

---
+++ lt_107.py
@@ -0,0 +1,44 @@
+
+# same as question 102
+# reverse result before return, so I copy the same code from 102
+from collections import deque
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        # BFS need queue to maitain level order
+        q = deque()
+
+        # My coding style to append root first
+        q.append(root)
+        
+        # create data structure to hold result
+        result = []
+        
+        while len(q) > 0:
+            # standard BFS operation: how many nodes in current level
+            nodes_count = len(q)
+            
+            # Specific for this question: need to save nodes from current level
+            current_level_nodes = []
+            
+            for i in range(nodes_count):
+                # standard BFS operation: get node from current level
+                node = q.popleft()
+
+                if node != None:
+                    # Standard BFS operation: append child to queue which will be the nodes in next level
+                    q.append(node.left)
+                    q.append(node.right)
+                
+                    # put node into current level, according to example, ignore leaf node
+                    current_level_nodes.append(node.val)
+            
+            # append nodes of current level to result
+            if len(current_level_nodes) > 0:
+                result.append(current_level_nodes)
+        
+        return result[::-1]
+        
